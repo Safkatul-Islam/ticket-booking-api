00b3d60725c16e0f84b0ac13607f6a94bf4a5cce
Md Safkatul Islam <mrsafkatul.stu24@gmail.com>
2026-01-17 23:41:52 -0800

    add the authentication controller for user registration

--- src/main/java/com/ticketease/ticket_booking_api/controller/AuthController.java
+++ src/main/java/com/ticketease/ticket_booking_api/controller/AuthController.java
@@ -1,4 +1,27 @@
 package com.ticketease.ticket_booking_api.controller;
 
+import com.ticketease.ticket_booking_api.dto.RegisterRequest;
+import com.ticketease.ticket_booking_api.service.AuthService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterRequest request) {
+        String response = authService.register(request);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
 }
